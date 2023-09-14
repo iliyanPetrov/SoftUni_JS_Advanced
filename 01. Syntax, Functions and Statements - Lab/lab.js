6ca57560606995e9da12c4053b5752574ec479b0
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-09-14 23:44:06 +0300

    so far so good

--- 01. Syntax, Functions and Statements - Lab/lab.js
+++ 01. Syntax, Functions and Statements - Lab/lab.js
@@ -8,10 +8,90 @@
 
 //-----------------------------
 
-function stringLength(str1, str2, str3) {
-    let sum = [str1, str2, str3].reduce((acc, str) => {
-        return acc + str.length;
-    }, 0);
-    console.log(sum)
+// function stringLength(str1, str2, str3) {
+//     let sum = [str1, str2, str3].reduce((acc, str) => {
+//         return acc + str.length;
+//     }, 0);
+//     console.log(sum)
+//     console.log(Math.floor(sum/3))
+// }
+// stringLength('chocolate', 'ice cream', 'cake');
+
+//-------------------------------
+
+// function largestNumber(one, two, three) {
+//     let result = Math.max(one, two, three);
+//     console.log(`The largest number is ${result}.`);
+// }
+// largestNumber(1, 2, 3);
+
+//------------------------------
+
+// function circleArea(input){
+//     let type = typeof input;
+//     let result;
+//     if (type == "number"){
+//         result = Math.pow(input, 2) * Math.PI;
+//         console.log(result.toFixed(2))
+//     }else{
+//         console.log(`We can not calculate the circle area, because we receive a ${type}.`)
+//     }
+// }
+
+// circleArea(5)
+
+//------------------------------
+
+// function mathOperations(num1, num2, operator){
+
+//     let result;
+//     switch(operator){
+//         case '+': result = num1+num2; break;
+//         case '-': result = num1-num2; break;
+//         case '*': result = num1*num2; break;
+//         case '/': result = num1/num2; break;
+//         case '%': result = num1%num2; break;
+//         case '**': result = num1**num2; break;
+//     }
+//     console.log(result);
+// }
+
+//------------------------------
+
+// function sumOfNumbers(n, m){
+
+//     let num1 = +n, num2 = +m, result = 0;
+//     for (let i = num1; i <= num2; i++) {
+//         result += i;
+//     } return console.log(result);
+// }
+
+// sumOfNumbers('-8', 20)
+
+//------------------------------
+
+// function dayOfWeek(day){
+//     let result;
+
+//     switch(day){
+//         case "Monday":  result = 1; break;
+//         case "Tuesday":  result = 2; break;
+//         case "Wednesday":  result = 3; break;
+//         case "Thursday":  result = 4; break;
+//         case "Friday":  result = 5; break;
+//         case "Saturday":  result = 6; break;
+//         case "Sunday":  result = 7; break;
+//         default: result = 'error'; break;
+//     }
+//     console.log(result)
+// }
+
+// dayOfWeek(1)
+
+//-------------------------------
+
+function daysInAMonth(month, year){
+    let i;
 }
-stringLength('abc', 'defg', 'qwerty')
+
+daysInAMonth(1, 2021)
---
+++ Syntax, Functions and Statements - Exercise/exercise.js
@@ -0,0 +1,29 @@
+// function fruit(fruit, grams, price) {
+//     let weight = grams / 1000;
+//     let money = weight * price;
+
+//     console.log(`I need ${money.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruit}.`)
+// }
+
+// fruit('apple', 1563, 2.35)
+
+/// TODO ------------
+// function previousDay(day, month, year){
+//     let myDate =
+// }
+
+// previousDay()------------
+
+
+//----------------------
+// function timeToWalk(steps, footPrint, speed) {
+//     let distanceInMeters = steps * footPrint;
+//     let speedMetersPerSec = speed / 3.6;
+//     let time = distanceInMeters / speedMetersPerSec;
+//     let rest = Math.floor(distanceInMeters / 500);
+
+//     // TODO
+
+// }
+// timeToWalk(4000, 0.6, 5);                  
+                                                                                                                                                                                                                                                                                                                                                                   
