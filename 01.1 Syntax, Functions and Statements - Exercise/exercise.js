9571519d7fbf15dec94782db7dfc0ec7233bcc06
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-09-16 20:53:38 +0300

    4 down, six more to go...

--- 01.1 Syntax, Functions and Statements - Exercise/exercise.js
+++ 01.1 Syntax, Functions and Statements - Exercise/exercise.js
@@ -1,3 +1,4 @@
+//      == 1 ==
 // function fruit(fruit, grams, price) {
 //     let weight = grams / 1000;
 //     let money = weight * price;
@@ -6,24 +7,61 @@
 // }
 
 // fruit('apple', 1563, 2.35)
+//---------------------------^^READY
 
-/// TODO ------------
-// function previousDay(day, month, year){
-//     let myDate =
+//      == 2 ==
+// function greatestCommonDevisor(a, b) {
+//     let gcd = a % b;
+//     while (gcd !== 0) {
+//         a = b;
+//         b = gcd;
+//         gcd = a % b;
+//     }
+//     console.log(b)
 // }
 
-// previousDay()------------
+// greatestCommonDevisor(15, 5);
+// greatestCommonDevisor(2154, 458);
 
+//---------------------------^^READY
 
-//----------------------
-// function timeToWalk(steps, footPrint, speed) {
-//     let distanceInMeters = steps * footPrint;
-//     let speedMetersPerSec = speed / 3.6;
-//     let time = distanceInMeters / speedMetersPerSec;
-//     let rest = Math.floor(distanceInMeters / 500);
-
-//     // TODO
-
+//      == 3 ==
+// function sameNumbers(input) {
+//     input = "" + input;
+//     let arr = input.split("");
+//     let last = arr[arr.length - 1];
+//     console.log(arr.every((el) => el == last));
+//     arr = arr.map(Number);
+//     let sum = 0;
+//     for (const num of arr) {
+//         sum+=num
+//     }
+//     console.log(sum)
 // }
-// timeToWalk(4000, 0.6, 5);                  
-                                                                                                                                                                                                                                                                                                                                                                   
+
+// sameNumbers(2222222);
+//---------------------------^^READY
+
+
+//      == 4 ==
+// function previousDay(year, month, day){
+
+//     let myDate = new Date(year, month -1, day);
+//     myDate.setDate(myDate.getDate() - 1);
+//     console.log(`${myDate.getFullYear()}-${myDate.getMonth()+1}-${myDate.getDate()}`)
+// }
+
+// previousDay(2016, 9, 1)
+//---------------------------^^READY
+
+
+//      == 5 ==
+function timeToWalk(steps, footPrint, speed) {
+    let distanceInMeters = steps * footPrint;
+    let speedMetersPerSec = speed / 3.6;
+    let time = distanceInMeters / speedMetersPerSec;
+    let rest = Math.floor(distanceInMeters / 500);
+
+
+}
+timeToWalk(4000, 0.6, 5);
