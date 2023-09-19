3e4d44a5c5c80948321f27b8946e36bbd5054a3f
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-09-20 00:38:32 +0300

    first done, at least

---
+++ 02. Arrays and Nested Arrays - Lab/lab.js
@@ -0,0 +1,21 @@
+
+//      == 1 ==
+// function evenPositionElements(arr) {
+//     let result = '';
+//     arr.forEach((element, i) => {
+//         if (i%2 == 0) result += element + ' '
+//     });
+//     console.log(result.slice(0, result.length - 1))
+// }
+
+// evenPositionElements(["20", "30", "40", "50", "60"]);
+// ----------------------^^Ready
+
+
+//      == 2 ==
+function lastKNumbersSequence(input){
+
+    let i;
+}
+
+lastKNumbersSequence(6, 3)
