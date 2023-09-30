a2217b505136ef60825372cf4eab2e6eebed6c52
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-09-30 22:23:28 +0300

    all obligatory tasks ready

--- 03.1 Objects and Composition - Excercise/exercise.js
+++ 03.1 Objects and Composition - Excercise/exercise.js
@@ -129,10 +129,142 @@
 
 
 //      == 6 ==
-function storeCatalogue(input){
+// function storeCatalog(inputArr){
 
-    let i;
+//     let result = {};
+
+//     for (const element of inputArr) {
+//         let [productName, price] = element.split(' : ');
+//         price = +price;
+//         // console.log(productName, price)
+//         const firstLetter = productName[0]
+//         // console.log(firstLetter)
+//         if(!result.hasOwnProperty(firstLetter)){
+//             result[firstLetter] = [];
+//         }
+//         result[firstLetter].push(`${productName}: ${price}`);
+//     }
+
+//     const resKeys = Object.keys(result).sort();
+//     // console.log(resKeys)
+//     for (const letter of resKeys) {
+//         console.log(letter);
+//         for (const element of result[letter].sort()) {
+//             console.log(`  ${element}`)
+//         }
+//     }
+// }
+// storeCatalog([
+//     "Appricot : 20.4",
+//     "Fridge : 1500",
+//     "TV : 1499",
+//     "Deodorant : 10",
+//     "Boiler : 300",
+//     "Apple : 1.25",
+//     "Anti-Bug Spray : 15",
+//     "T-Shirt : 10",
+// ]);
+// storeCatalog([
+//     "Banana : 2",
+//     "Rubic's Cube : 5",
+//     "Raspberry P : 4999",
+//     "Rolex : 100000",
+//     "Rollon : 10",
+//     "Rali Car : 2000000",
+//     "Pesho : 0.000001",
+//     "Barrel : 10",
+// ]);
+//-------------------------^^Ready
+
+
+//      == 7 ==
+// function townsToJSON(inputArr){
+
+//     let result = [];
+//     let [townField, latitudeField, longitudeField] = inputArr.shift().match(/[a-zA-Z]+/g);
+
+//     for (const line of inputArr) {
+//         const [town, latitude, longitude] = line.slice(2, line.length - 2).split(' | ');
+//         result.push({
+//             [`${townField}`]: town,
+//             [`${latitudeField}`]: +Number(latitude).toFixed(2),
+//             [`${longitudeField}`]: +Number(longitude).toFixed(2)
+//         })
+//     }
+//     return JSON.stringify(result)
+//     // console.log(JSON.stringify(result))
+// }
+
+// // townsToJSON([
+// //     "| Town | Latitude | Longitude |",
+// //     "| Sofia | 42.696552 | 23.32601 |",
+// //     "| Beijing | 39.913818 | 116.363625 |",
+// // ]);
+// townsToJSON([
+//     '| Town | Latitude | Longitude |',
+//     '| Veliko Turnovo | 43.0757 | 25.6172 |',
+//     '| Monatevideo | 34.50 | 56.11 |'
+//   ])
+//-------------------------^^Ready
+
+
+//      == 8 ==
+// function rectangle(width, height, color){
+//     color = color[0].toUpperCase() + color.slice(1)
+//     let result = {
+//         width,
+//         height,
+//         color,
+//         calcArea: function() {
+//             return this.width * this.height
+//         }
+//     }
+//     return result
+// }
+// let rect = rectangle(4, 5, "red");
+// console.log(rect.width);
+// console.log(rect.height);
+// console.log(rect.color);
+// console.log(rect.calcArea());
+//-------------------------^^Ready
+
+
+//      == 10 ==
+function heroes() {
+    return {
+        mage: function (name) {
+            return {
+                name,
+                health: 100,
+                mana: 100,
+                cast: function (spellName) {
+                    this.mana -= 1;
+                    console.log(`${this.name} cast ${spellName}`);
+                },
+            };
+        },
+        fighter: function (name) {
+            return {
+                name,
+                health: 100,
+                stamina: 100,
+                fight: function () {
+                    this.stamina -= 1;
+                    console.log(`${this.name} slashes at the foe!`);
+                },
+            };
+        },
+    };
 }
 
-storeCatalogue()
-// storeCatalogue()
+let create = heroes();
+const scorcher = create.mage("Scorcher");
+scorcher.cast("fireball");
+scorcher.cast("thunder");
+scorcher.cast("light");
+
+const scorcher2 = create.fighter("Scorcher 2");
+scorcher2.fight();
+
+console.log(scorcher2.stamina);
+console.log(scorcher.mana);
