0714cdf028dc37a83b061520cc171bccf75d7d61
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-09-30 17:48:03 +0300

    5 tasks ready, still more to go

--- 03.1 Objects and Composition - Excercise/exercise.js
+++ 03.1 Objects and Composition - Excercise/exercise.js
@@ -35,18 +35,104 @@
 
 
 //      == 3 ==
-function carFactory(input) {
-    let result = {
-        model: `${input.model}`,
+// function carFactory(input) {
 
-    }
-    let power = input.power;
-    console.log(power > 90 ? input.power : 120 ? 200 : null)
+//     let power = input.power;
+//     let carType = input.carriage;
+//     let carCoupe = input.color;
+//     let sizeCheck = input.wheelsize;
+//     let wheels = sizeCheck % 2 !== 0 ? sizeCheck : sizeCheck-1;
+
+//     let result = {
+//         "model": `${input.model}`,
+//         "engine": {
+//             "power": (power <= 90 ? 90 : power <= 120 ? 120 : 200),
+//             "volume": (power<= 90 ? 1800 : power <= 120 ? 2400 : 3500)
+//         },
+//         "carriage": {
+//             "type": carType,
+//             "color": carCoupe
+//         },
+//         "wheels": [wheels, wheels, wheels, wheels]
+//     }
+//     // console.table(result)
+//     return result;
+// }
+// carFactory({
+//     model: "VW Golf II",
+//     power: 90,
+//     color: "blue",
+//     carriage: "hatchback",
+//     wheelsize: 14,
+// });
+//-------------------------^^Ready
+
+
+//      == 4 ==
+// function heroInventory(inputArr){
+//     let result = [];
+//     for (const line of inputArr) {
+//         let [name, level, items] = line.split(" / ");
+//         // console.log(`name=${name}, level=${level}, items=${items}`)
+//         level = Number(level);
+//         items = items ? items.split(', ') : [];
+//         // console.log(items)
+//         result.push({ name, level, items });
+//     }
+//     console.log(JSON.stringify(result))
+// }
+
+// heroInventory([
+//     "Isacc / 25 / Apple, GravityGun",
+//     "Derek / 12 / BarrelVest, DestructionSword",
+//     "Hes / 1 / Desolator, Sentinel, Antara",
+// ]);
+// heroInventory(["Jake / 1000 / Gauss, HolidayGrenade"]);
+//-------------------------^^Ready
+
+
+//      == 5 ==
+// function lowestPriceInTown(inputArr) {
+//     let result = new Map();
+    
+//     for (const element of inputArr) {
+//         let [town, product, price] = element.split(' | ');
+//         price = Number(price);
+//         if (!result.has(product)) {
+//             result.set(product, {town, price});
+//         }
+//         let lowestPrice = result.get(product).price;
+//         if (lowestPrice > price) {
+//             result.set(product, {town, price})
+//         }
+//     }
+//     for (const [key, value] of result) {
+//         const product = key;
+//         const town = value.town;
+//         const price = value.price
+//         console.log(`${product} -> ${price} (${town})`);
+//     }
+// }
+// lowestPriceInTown([
+//     "Sofia City | BMW | 100000",
+//     "Sofia City | Audi | 100000",
+//     "Sofia City | Mitsubishi | 10000",
+//     "Sofia City | Mercedes | 10000",
+//     "Sofia City | NoOffenseToCarLovers | 0",
+//     "Mexico City | Audi | 1000",
+//     "Mexico City | BMW | 99999",
+//     "Mexico City | Mitsubishi | 10000",
+//     "New York City | Mitsubishi | 1000",
+//     "Washington City | Mercedes | 1000",
+// ]);
+//-------------------------^^Ready
+
+
+//      == 6 ==
+function storeCatalogue(input){
+
+    let i;
 }
-carFactory({
-    model: "VW Golf II",
-    power: 90,
-    color: "blue",
-    carriage: "hatchback",
-    wheelsize: 14,
-});
+
+storeCatalogue()
+// storeCatalogue()
