d4ee4e3d794974e62fe5e3910d658102ecf64143
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-10-07 10:58:29 +0300

    first task done, made some css changes

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    console.log('TODO:...');
+    const num1 = Number(document.querySelector("div#wrapper :first-child").value);
+    const num2 = Number(document.querySelector("div#wrapper :nth-child(2)").value);
+    const result = num1 - num2;
+    document.querySelector("div#wrapper :last-child").textContent = result;
 }
