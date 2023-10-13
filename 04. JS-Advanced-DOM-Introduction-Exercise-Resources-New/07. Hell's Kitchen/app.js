2a428acb7d0ac705e481711c7a946e44bda74d8d
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-10-13 09:40:58 +0300

    7 and 8 task ready - enough

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -1,8 +1,47 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
+    const restaurants = new Map();
+    function onClick () {
+        const inputArr = JSON.parse(document.querySelector('textarea').value);
+        for (const line of inputArr) {
+            let [restaurantName, workersStr] = line.split(' - ');
+            let workersArr = workersStr.split(', ');
+            if(!restaurants.has(restaurantName)){
+                restaurants.set(restaurantName, workersArr);
+            }else{
+                let oldWorkers = restaurants.get(restaurantName);
+                let addedWorkers = oldWorkers.concat(workersArr);
+                restaurants.set(restaurantName, addedWorkers);
+            }
+        }
+        let bestRes = '';
+        let higAvgSalary = 0;
+        for (const [name, value] of restaurants) {
+            let sumSal = value.reduce((acc, curr) => {
+                return acc + Number(curr.split(" ")[1]);
+            }, 0);
+            const avgSal = sumSal / value.length;
+            if (higAvgSalary<avgSal){
+                higAvgSalary = avgSal;
+                bestRes = name;
+            }
+        }
+        const sortedWorkers = restaurants.get(bestRes).sort((a, b) => {
+            let salA = Number(a.split(" ")[1]);
+            let salB = Number(b.split(" ")[1]);
+            return salB - salA;
+        });
+        let bestWorkSal = Number(sortedWorkers[0].split(" ")[1]);
+        let workersArrStr = ""
+        for (const el of sortedWorkers) {
+            const [name, salary] = el.split(' ')
+            workersArrStr += `Name: ${name} With Salary: ${salary} `
+        }
+        // DISPLAY RESULTS
+        document.querySelector('#bestRestaurant p')
+        .textContent = `Name: ${bestRes} Average Salary: ${higAvgSalary.toFixed(2)} Best Salary: ${bestWorkSal.toFixed(2)}`
+        document.querySelector('#workers p')
+        .textContent = workersArrStr.trim();
    }
 }
--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,23 @@
 function generateReport() {
-    //TODO
+    const inputNames = Array.from(document.querySelector('thead tr').querySelectorAll('th'));
+    const len = inputNames.length;
+    const resultArr = [];
+    const numOfObjects = document.querySelector('tbody').querySelectorAll('tr').length;
+    for (let w = 0; w < numOfObjects; w++){
+        resultArr[w] = {};
+    }
+    const dataList = Array.from(document.querySelector('tbody').querySelectorAll('tr'));
+    for (let i = 0; i<len; i++){
+        const element = inputNames[i].firstElementChild;
+        if (element.checked == true){
+            // FOROF
+            let currPropName = element.name;
+            for (let z = 0; z < numOfObjects; z++){
+                const innerData = Array.from(dataList[z].querySelectorAll('td'));
+                resultArr[z][currPropName] = innerData[i].textContent;
+            }
+        }
+    }
+    const output = JSON.stringify(resultArr)
+    document.querySelector('#output').textContent = output;
 }
