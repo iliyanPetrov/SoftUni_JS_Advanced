af8ee0346498f633a0cc96ba0c880a1af171afd2
iliyanPetrov <iliyan.petrov1986@gmail.com>
2023-10-14 10:34:28 +0300

    its a new day (1-3 tasks ready)

--- 05.01. Sections_Resources/02. Time-Converter/solution.js
+++ 05.01. Sections_Resources/02. Time-Converter/solution.js
@@ -1,4 +1,23 @@
 function attachEventsListeners() {
-
-    console.log('TODO:...');
+    const buttonsArr = Array.from(
+        document.querySelectorAll("input[type=button]")
+    ).forEach((btn) => btn.addEventListener("click", clickHandler));
+    const inputsArr = Array.from(document.querySelectorAll('input[type=text]'));
+    debugger
+    function clickHandler(e){
+        const numValue = Number(e.target.previousElementSibling.value);
+        const unit = e.target.previousElementSibling.id;
+        switch (unit){
+            case "days": calcValues(numValue); break;
+            case "hours": calcValues(numValue / 24); break;
+            case "minutes": calcValues(numValue / 24 / 60); break;
+            case "seconds": calcValues(numValue / 24 / 60 / 60); break;
+        }
+    }
+    function calcValues(value){
+        inputsArr[0].value = value;
+        inputsArr[1].value = value*24;
+        inputsArr[2].value = value*24*60;
+        inputsArr[3].value = value*24*60*60;
+    }
 }
--- 05.01. Sections_Resources/03. Locked-Profile/app.js
+++ 05.01. Sections_Resources/03. Locked-Profile/app.js
@@ -1,3 +1,20 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttonsRef = Array.from(document.querySelectorAll('button'));
+    for (const element of buttonsRef) {
+        element.addEventListener("click", handleShowMore);
+    }
+    function handleShowMore(e){
+        const button = e.target
+        const unlocked = e.target.parentElement.querySelector('input[value=unlock]').checked;
+        const hiddenDiv = e.target.previousElementSibling;
+        
+        if(unlocked && button.textContent == "Show more"){
+            hiddenDiv.style.display = "block";
+            button.textContent = "Hide it";
+        }else if(unlocked && button.textContent == "Hide it"){
+            hiddenDiv.style.display = "none";
+            button.textContent = "Show more";
+        }
+    }
+    const radioInputRef = document.querySelector('input[value=unlock]');
 }
